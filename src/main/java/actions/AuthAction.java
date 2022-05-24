6ca3fe4b12429999aa1d4de105216f1c05b01050
happy <happy@HAPPY-11700>
2022-05-24 21:29:09 +0900

    ログイン画面表示アクションの作成

---
+++ src/main/java/actions/AuthAction.java
@@ -0,0 +1,55 @@
+package actions;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+
+import constants.AttributeConst;
+import constants.ForwardConst;
+import services.EmployeeService;
+
+/**
+ * 認証に関する処理を行うActionクラス
+ *
+ */
+
+public class AuthAction extends ActionBase{
+
+    private EmployeeService service;
+
+    /**
+     * メソッドを実行する
+     */
+    @Override
+    public void process() throws ServletException, IOException {
+
+        service = new EmployeeService();
+
+        //メソッドを実行
+        invoke();
+
+        service.close();
+    }
+
+    /**
+     * ログイン画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void showLogin() throws ServletException, IOException {
+        //CSRF対策用トークンを設定
+        putRequestScope(AttributeConst.TOKEN,getTokenId());
+
+        //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        //一覧画面を表示
+        forward(ForwardConst.FW_LOGIN);
+
+
+    }
+
+}
